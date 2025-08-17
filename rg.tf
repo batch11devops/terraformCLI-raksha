46adaf2d418a2d75cede649dfab8c1530082477c
batch11devops <rakshashetty722@gmail.com>
2025-08-17 17:14:58 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -22,7 +22,7 @@ provider "azurerm"{
   features{}
 client_id="7f7f227d-6494-412a-9b90-3dd99fa461a5"
 tenant_id="a1099c37-a63e-49aa-aae9-c1c91be6b89a"
-subscription_id="972f43d4-e847-43f2-8efe-0f9c1507d61e"
+
 client_secret="cmU8Q~zmApcyQ8oN9z3xhwGV-VTPqfm~8IlXRb5Z"
 }
 
